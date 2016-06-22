f3a8e647c1dc7f1126d18d126f5f1a697affcba0
Tobias <qenzoz@gmail.com>
2016-06-22 17:09:29 +0200

    Setup BaseActivity

---
+++ app/src/main/java/com/example/tobias/recipist/BaseActivity.java
@@ -0,0 +1,33 @@
+package com.example.tobias.recipist;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Tobias on 22-06-2016.
+ */
+public class BaseActivity extends AppCompatActivity {
+    private ProgressDialog mProgressDialog;
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.base_activity_progress_dialog_loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.hide();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        hideProgressDialog();
+    }
+}
